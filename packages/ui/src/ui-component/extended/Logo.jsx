d8cc95bbcf4b873a74c299b2fdd2916d519bfb53
Niels Wilke <niels.wilke@tauversicht.tech>
2024-12-17 12:47:41 +0100

    Fixed the logo usage

--- packages/ui/src/ui-component/extended/Logo.jsx
+++ packages/ui/src/ui-component/extended/Logo.jsx
@@ -1,5 +1,5 @@
-import logo from '@/assets/images/flowise_logo_dark.png'
-import logoDark from '@/assets/images/flowise_logo.png'
+import logo from '@/assets/images/snaic_logo.png'
+import logoDark from '@/assets/images/snaic_logo_dark.png'
 
 import { useSelector } from 'react-redux'
 
